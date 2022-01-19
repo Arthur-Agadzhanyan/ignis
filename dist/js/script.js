c55d497ecd114583521f71d9145dc3a23e34a1aa
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-01-19 16:29:39 +0300

    added: mobile navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,15 +17,24 @@ function testWebP(callback) {
 });;
 
 const introTitle = document.querySelector('.intro__title');
+
 const navigation = document.getElementById('navigation')
+const navigationMb = document.querySelector('.navbar_mobile')
+
 const introInfo = document.querySelector('.intro__info')
 const introContent = document.querySelector('.intro__content')
 const listLinks = document.querySelectorAll('.list__link')
+
+const navbarBtn = document.getElementById("navbar__btn")
+const mobileBar = document.querySelector(".mobile_bar")
+
+window.scrollTo(0, 0)
 
 introTitle.addEventListener('click',()=>{
     window.scrollTo(0, 0)
     document.body.classList.toggle("scroll_block")
     navigation.classList.toggle("nav_closed")
+    navigationMb.classList.toggle("nav_closed")
 
     introInfo.classList.toggle("info_opened")
     introContent.classList.toggle("content_opened")
@@ -35,6 +44,13 @@ introTitle.addEventListener('click',()=>{
     })
 })
 
+navbarBtn.addEventListener('click',(e)=>{
+
+    e.currentTarget.classList.toggle('burger_active')
+
+    mobileBar.classList.toggle("mb_bar_closed")
+})
+
 listLinks.forEach((link)=>{
 
     link.addEventListener('click',(e)=>{
@@ -42,6 +58,8 @@ listLinks.forEach((link)=>{
             item.classList.remove('list__link-active')
         })
         e.currentTarget.classList.add("list__link-active")
+        mobileBar.classList.add("mb_bar_closed")
+        navbarBtn.classList.remove('burger_active')
     })
 
 })
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,24 @@
 @@include('webpTest.js');
 
 const introTitle = document.querySelector('.intro__title');
+
 const navigation = document.getElementById('navigation')
+const navigationMb = document.querySelector('.navbar_mobile')
+
 const introInfo = document.querySelector('.intro__info')
 const introContent = document.querySelector('.intro__content')
 const listLinks = document.querySelectorAll('.list__link')
+
+const navbarBtn = document.getElementById("navbar__btn")
+const mobileBar = document.querySelector(".mobile_bar")
+
+window.scrollTo(0, 0)
 
 introTitle.addEventListener('click',()=>{
     window.scrollTo(0, 0)
     document.body.classList.toggle("scroll_block")
     navigation.classList.toggle("nav_closed")
+    navigationMb.classList.toggle("nav_closed")
 
     introInfo.classList.toggle("info_opened")
     introContent.classList.toggle("content_opened")
@@ -19,6 +28,13 @@ introTitle.addEventListener('click',()=>{
     })
 })
 
+navbarBtn.addEventListener('click',(e)=>{
+
+    e.currentTarget.classList.toggle('burger_active')
+
+    mobileBar.classList.toggle("mb_bar_closed")
+})
+
 listLinks.forEach((link)=>{
 
     link.addEventListener('click',(e)=>{
@@ -26,6 +42,8 @@ listLinks.forEach((link)=>{
             item.classList.remove('list__link-active')
         })
         e.currentTarget.classList.add("list__link-active")
+        mobileBar.classList.add("mb_bar_closed")
+        navbarBtn.classList.remove('burger_active')
     })
 
 })
